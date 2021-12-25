53a6bfaa56bfc08a6dceeda42eace9a1ff308861
Jeep Kiddee <nutthawut.ki@gmail.com>
2021-12-25 22:13:13 +0700

    Settings docker and db

--- backend/motorplus/settings.py
+++ backend/motorplus/settings.py
@@ -12,9 +12,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -74,10 +83,8 @@ WSGI_APPLICATION = 'motorplus.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # read os.environ['DATABASE_URL']
+    'default': env.db()
 }
 
 
